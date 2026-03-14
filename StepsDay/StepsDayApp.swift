2e42dc2813565a6857d9d772895e16d3ef44dd9f
K3nse1 <raulsangut999@gmail.com>
2026-03-14 10:20:44 +0100

    Initial Commit

---
+++ StepsDay/StepsDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepsDayApp.swift
+//  StepsDay
+//
+//  Created by Raúl Santos Gutiérrez on 14/3/26.
+//
+
+import SwiftUI
+
+@main
+struct StepsDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
